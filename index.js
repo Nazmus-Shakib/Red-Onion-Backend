37874f10527cd3c6cbcde16475cb392e30a4e647
Shakib16 <shakibnazmus16@gmail.com>
2020-05-12 01:17:41 +0800

    Problem with backend data loading

--- index.js
+++ index.js
@@ -17,21 +17,87 @@ let client = new MongoClient(uri, {
 
 // database connection:
 
-client.connect((err) => {
-  const collection = client.db("redOnionRestaurant").collection("foods");
-  // perform actions on the collection object
-  collection.insertOne(
-    {
-      name: "Fuchka",
-      price: 50,
-      quantity: 20,
-    },
-    (err, res) => {
-      console.log("successfully inserted");
-    }
-  );
-  console.log("database connected...");
-  client.close();
+// how get/load products data from server
+app.get("/foods", (req, res) => {
+  client = new MongoClient(uri, { useNewUrlParser: true });
+
+  client.connect((err) => {
+    const collection = client.db("redOnionRestaurant").collection("foods");
+    // perform actions on the collection object
+    collection.find().toArray((err, documents) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send({ message: err });
+      } else {
+        res.send(documents);
+      }
+    });
+
+    client.close();
+  });
+});
+
+// how get dynamic data of products key asked by user from server
+app.get("/food/:id", (req, res) => {
+  const id = req.params.id; // read user request
+
+  client = new MongoClient(uri, { useNewUrlParser: true });
+
+  client.connect((err) => {
+    const collection = client.db("redOnionRestaurant").collection("foods");
+    // perform actions on the collection object
+    collection.find({ id }).toArray((err, documents) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send({ message: err });
+      } else {
+        res.send(documents[0]);
+      }
+    });
+
+    client.close();
+  });
+});
+
+// client.connect((err) => {
+//   const collection = client.db("redOnionRestaurant").collection("foods");
+//   // perform actions on the collection object
+//   collection.insertOne(
+//     {
+//       name: "Fuchka",
+//       price: 50,
+//       quantity: 20,
+//     },
+//     (err, res) => {
+//       console.log("successfully inserted");
+//     }
+//   );
+//   console.log("database connected...");
+//   client.close();
+// });
+
+// how to post all product data to server
+app.post("/addFood", (req, res) => {
+  // save to database
+  const food = req.body;
+  client = new MongoClient(uri, {
+    useNewUrlParser: true,
+  });
+
+  client.connect((err) => {
+    const collection = client.db("redOnionRestaurant").collection("foods");
+    // perform actions on the collection object
+    collection.insert(food, (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send({ message: err });
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+
+    client.close();
+  });
 });
 
 const port = process.env.PORT || 3002;
